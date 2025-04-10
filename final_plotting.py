89404d07b0ecf4aee7504f8a7cfee0e8af45757b
sofiesana <119871294+sofiesana@users.noreply.github.com>
2025-04-10 10:49:24 +0200

    bar plot working

--- final_plotting.py
+++ final_plotting.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from scipy.interpolate import interp1d
+import pandas as pd
+import re
+import seaborn as sns
 
 def get_dataset_name(dataset, ood_type):
     if ood_type == "Near OOD" and dataset == "MNIST":
@@ -15,11 +18,11 @@ def get_dataset_name(dataset, ood_type):
 
     return other_dataset_name
 
-def load_roc(dataset, method, ood_type):
+def load_roc(dataset, method, ood_type, iter_num=1):
     """Load ROC curve data from a saved .npz file"""
     folder_path = f"Saved Rocks/{method}/{dataset}/"
     other_dataset_name = get_dataset_name(dataset, ood_type)
-    file_name = f"{method} -- {dataset} (ID) vs {other_dataset_name} ({ood_type}) {str(1)}.npz"
+    file_name = f"{method} -- {dataset} (ID) vs {other_dataset_name} ({ood_type}) {str(iter_num)}.npz"
     data = np.load(os.path.join(folder_path, file_name))
     return data['fpr'], data['tpr']
 
@@ -35,6 +38,67 @@ def load_all_rocs(dataset):
             rocs.append((fpr, tpr, dataset, method, ood_type))
 
     return rocs
+
+def get_ood_dataset_name(filename):
+    match = re.search(r" vs (.+?) \(", filename)
+    ood_dataset = match.group(1) if match else print(filename, "NO OOD DATASET FOUND")
+    if ood_dataset == "CIFAR100" or ood_dataset == "EMNIST":
+        distance = "Near OOD"
+    elif ood_dataset == "SVHN" or ood_dataset == "Fashion MNIST" or ood_dataset == "FashionMNIST":
+        if ood_dataset == "Fashion MNIST":
+            ood_dataset = "FashionMNIST"
+        distance = "Far OOD"
+    return ood_dataset, distance
+
+def make_dataframe():
+    # Create a DataFrame to store the results
+    rows = []
+    results_path = "./Saved Rocks"
+    methods = os.listdir(results_path)
+    for method in methods:
+        method_path = os.path.join(results_path, method)
+        datasets = os.listdir(method_path)
+        for dataset in datasets:
+            dataset_path = os.path.join(method_path, dataset)
+            files = os.listdir(dataset_path)
+            for file in files:
+                if file.endswith(".npz"):
+                    print(file)
+                    ood_dataset, distance = get_ood_dataset_name(file)
+                    data = np.load(os.path.join(dataset_path, file))
+                    fpr = data['fpr']
+                    tpr = data['tpr']
+                    roc_auc = np.trapz(tpr, fpr)
+                    # print(f"Method: {method}, ID Dataset: {dataset}, OOD Dataset: {ood_dataset}, AUROC: {roc_auc}, Distance: {distance}")
+                    rows.append({
+                        "Method": method,
+                        "ID Dataset": dataset,
+                        "OOD Dataset": ood_dataset,
+                        "AUROC": roc_auc,
+                        "Distance": distance
+                    })
+    df = pd.DataFrame(rows, columns=["Method", "ID Dataset", "OOD Dataset", "AUROC", "Distance"])
+    print(df[df["Method"] == "DUQ"])
+    return df
+
+def make_barplot(target_dataset=None):
+    df = make_dataframe()
+    # filter df to only include target_distance if specified
+    if target_dataset:
+        df = df[df["ID Dataset"] == target_dataset]
+    
+    fig, ax = plt.subplots(figsize=(6, 4))
+    sns.set_theme(style="whitegrid")
+    sns.barplot(
+        data=df,
+        x="Distance",
+        y="AUROC",
+        hue="Method"
+    )
+    ax.set_title(f"AUROC for Different Methods on {target_dataset} Dataset")
+    diff = df["AUROC"].max() - df["AUROC"].min()
+    ax.set(ylim=(min(df["AUROC"]-(0.1*diff)), 1))
+    plt.show()
 
 def plot_all_rocs():
     datasets = ["MNIST", "CIFAR10"]
@@ -67,4 +131,5 @@ def plot_all_rocs():
 
                 
 
-plot_all_rocs()
+# plot_all_rocs()
+make_barplot("MNIST")
